cba1ac90a65cab763102906e8cb8502afbef462c
nassi <anssi_puustinen@hotmail.com>
2017-10-08 04:57:09 +0300

    Fixed game roulette screen

--- src/gameRoulette.lua
+++ src/gameRoulette.lua
@@ -36,15 +36,23 @@ function gameRoulette:entering()
 end
 
 function gameRoulette:entered()
-	self.timer:add(1, function() Venus.switch(Game.minigames[Game.minigameNames[self.gameIndex] ]) end)
+	self.timer:add(3, function() Venus.switch(Game.minigames[Game.minigameNames[self.gameIndex] ]) end)
 end
 
 function gameRoulette:update(dt)
+	if Game.paused then return end
 	self.timer:update(dt)
 end
 
 function gameRoulette:draw()
-	drawMinigameInfo(self.gameIndex)
+	preDraw()
+	love.graphics.setBackgroundColor(30, 30, 30)
+	love.graphics.setColor(170, 170, 170)
+	local text = "NEXT GAME"
+	love.graphics.setFont(Game.font40)
+	love.graphics.print(text, Game.original.w/2 - Game.font40:getWidth(text)/2, 10)
+	drawMinigameInfo(self.gameIndex, { 70, 70, 70 }, { 200, 200, 200 })
+	postDraw()
 end
 
 return gameRoulette
